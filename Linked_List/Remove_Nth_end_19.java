0b34c53af6b00b5753048b56f3fd2c279084aa42
Abhishek Kumar Sah <abhisheksah2032004@gmail.com>
2026-05-26 21:31:10 +0530

    Solution of removeNthFromEnd

---
+++ Linked_List/Remove_Nth_end_19.java
@@ -0,0 +1,30 @@
+package Linked_List;
+
+public class Remove_Nth_end_19 {
+    // To find length
+    private int length(ListNode head){
+        int res = 0 ;
+        ListNode temp = head ;
+        while(temp != null){
+            temp = temp.next ;
+            res++;
+        }
+        return res ;
+    }
+    //Main function
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0) ;
+        dummy.next = head ;
+        int tot = length(head) ;
+        
+        int req = tot-n ;
+        ListNode temp = dummy ;
+        
+        for(int i = 0 ; i < req ; i++){
+            temp = temp.next ;
+        }
+        temp.next = temp.next.next ;
+        return dummy.next ;
+    }
+}
